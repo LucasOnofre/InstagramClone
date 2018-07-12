61dfe2801bae8ac1eda520a58e26f592b1da6f1d
Lucas Onofre Calisto Rosa <lucas-onofre@hotmail.com>
2018-07-12 16:17:09 -0300

    Criada a toolbar

--- ParseStarterProject/src/main/java/com/parse/starter/activity/LoginActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/activity/LoginActivity.java
@@ -7,7 +7,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.parse.LogInCallback;
+import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.starter.R;
 
@@ -23,10 +26,15 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        txtUsuario = (EditText) findViewById(R.id.box_usuario_login_id);
-        txtSenha = (EditText) findViewById(R.id.box_senha_login_id);
-        btnLogar = (Button) findViewById(R.id.btn_logar_id);
+        txtUsuario    = (EditText) findViewById(R.id.box_usuario_login_id);
+        txtSenha      = (EditText) findViewById(R.id.box_senha_login_id);
+        btnLogar      = (Button) findViewById(R.id.btn_logar_id);
         txtCriarConta = (TextView) findViewById(R.id.txt_faca_login_id);
+
+       //ParseUser.logOut();
+
+        //Listener do botão logar
+        btnLogar.setOnClickListener(listener_btn_logar);
 
         //Verifica se o usuário está logado
         verificaUsuarioLogado();
@@ -48,5 +56,36 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(LoginActivity.this,MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    private View.OnClickListener listener_btn_logar = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            String usuario = txtUsuario.getText().toString();
+            String senha   = txtSenha.getText().toString();
+
+            verificarLogin(usuario,senha);
+
+        }
+    };
+
+    private void verificarLogin(String usuario, String senha) {
+
+        ParseUser.logInInBackground(usuario, senha, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+
+                //Sucesso no login
+                if (e == null){
+                    Toast.makeText(LoginActivity.this, "Login com sucesso", Toast.LENGTH_SHORT).show();
+                    abrirTelaPrincipal();
+
+                    //Erro ao logar
+                }else{
+                    Toast.makeText(LoginActivity.this, "Erro ao fazer login", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- ParseStarterProject/src/main/java/com/parse/starter/activity/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/activity/MainActivity.java
@@ -7,75 +7,61 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 package com.parse.starter.activity;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
-import com.parse.FindCallback;
-import com.parse.GetCallback;
-import com.parse.LogInCallback;
-import com.parse.Parse;
-import com.parse.ParseAnalytics;
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
 import com.parse.ParseUser;
-import com.parse.SaveCallback;
-import com.parse.SignUpCallback;
 import com.parse.starter.R;
-
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+
+  private Toolbar toolbarPrincipal;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    /** Cadastra o usuário **/
-//    ParseUser usuario = new ParseUser();
-//    usuario.setUsername("lucasonofre");
-//    usuario.setPassword("123456");
-//    usuario.setEmail("jamiltoncursos@gmail.com");
-//
-//    usuario.signUpInBackground(new SignUpCallback() {
-//      @Override
-//      public void done(ParseException e) {
-//
-//        //Verifica se o erro é vazio, se for, é pq ta certo
-//        if (e == null){
-//          Log.i("cadastroUsuario","Erro ao cadastrar usuário - " + e.getMessage());
-//        }
-//      }
-//    });
-
-      /** verifica o usuário logado**/
-//    //verifica se está logado
-//    if(ParseUser.getCurrentUser() != null){
-//      Log.i("Loginusuário","Usuário logado");
-//    }else
-//      Log.i("Loginusuário","Usuário não logado");
-//
-
-//    //logar
-//    ParseUser.logInInBackground("lucasonofre", "14587", new LogInCallback() {
-//      @Override
-//      public void done(ParseUser user, ParseException e) {
-//
-//        if (e == null){
-//          Log.i("verificaLoginUsuario "," Login sucesso ");
-//        }else
-//          Log.i("verificaLoginUsuario "," Erro ao fazer login" + e.getMessage());
-//      }
-//    });
-
-    //deslogar
-    //ParseUser.logOut();
+          toolbarPrincipal = (Toolbar) findViewById(R.id.toolbar_principal);
+          toolbarPrincipal.setTitle("");
+          toolbarPrincipal.setLogo(R.drawable.instagramlogo);
+          setSupportActionBar(toolbarPrincipal);
   }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    //Listener do menu da toolbar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+      switch (item.getItemId()){
+
+          case R.id.action_sair:
+              deslogarUsuario();
+              return true;
+          case R.id.action_share:
+              return true;
+          case R.id.action_configuracoes:
+              return true;
+              default:
+                  return super.onOptionsItemSelected(item);
+      }
+    }
+
+    private void deslogarUsuario() {
+        ParseUser.logOut();
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
 }
